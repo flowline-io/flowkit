93ecb9237b6c76aa970282522838b3b569bc714b
sysatom <sysatom@gmail.com>
2023-10-15 11:58:43 +0800

    settings

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import {ThemeProvider} from "@/components/theme-provider";
-import DashboardPage from "@/pages/dashboard/Dashboard";
+import SettingsLayout from "@/pages/settings/layout";
+import SettingsNotificationsPage from "@/pages/settings/notifications/page";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <DashboardPage />
+      <SettingsLayout children={<SettingsNotificationsPage />} />
     </ThemeProvider>
   )
 }
