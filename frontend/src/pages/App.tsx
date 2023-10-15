f8a9961dcdd306b68af59da86550a62a28fc75b0
sysatom <sysatom@gmail.com>
2023-10-15 11:21:00 +0800

    dashboard

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,81 +1,11 @@
-import {useState} from 'react';
-import logo from '@/assets/images/logo-universal.png';
 import './App.css';
-import {Greet} from "../../wailsjs/go/main/App";
-import {Button} from "@/components/ui/button";
-import * as z from "zod";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {useForm} from "react-hook-form";
-import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
 import {ThemeProvider} from "@/components/theme-provider";
-import {ModeToggle} from "@/components/mode-toggle";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+import DashboardPage from "@/pages/dashboard/Dashboard";
 
 function App() {
-  const [resultText, setResultText] = useState("Please enter your name below 👇");
-  const [name, setName] = useState('');
-  const updateName = (e: any) => setName(e.target.value);
-  const updateResultText = (result: string) => setResultText(result);
-
-  function greet() {
-    Greet(name).then(updateResultText);
-  }
-
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
-
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-  }
-
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <div id="App">
-        <img src={logo} id="logo" alt="logo"/>
-        <div id="result" className="result">{resultText}</div>
-        <ModeToggle/>
-        <div id="input" className="input-box">
-          <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-          <Button onClick={greet}>Click me</Button>
-        </div>
-        <div>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input placeholder="shadcn" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      This is your public display name.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Submit</Button>
-            </form>
-          </Form>
-        </div>
-      </div>
+      <DashboardPage />
     </ThemeProvider>
   )
 }
